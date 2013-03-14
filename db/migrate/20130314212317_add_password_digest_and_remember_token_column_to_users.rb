86e231f5bbcd30e3f841db467704ab14dd278191
Michael Morrissey <mikehmorrissey@gmail.com>
2013-03-14 17:35:23 -0400

    add database indices

--- db/migrate/20130314212317_add_password_digest_and_remember_token_column_to_users.rb
+++ db/migrate/20130314212317_add_password_digest_and_remember_token_column_to_users.rb
@@ -2,5 +2,6 @@ class AddPasswordDigestAndRememberTokenColumnToUsers < ActiveRecord::Migration
   def change
     add_column :users, :password_digest, :string
     add_column :users, :remember_token, :string
+   
   end
 end
---
+++ db/migrate/20130314213331_addindex.rb
@@ -0,0 +1,11 @@
+class Addindex < ActiveRecord::Migration
+  def up
+  	add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
+  end
+
+  def down
+  	remove_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  remove_index "users", ["remember_token"], :name => "index_users_on_remember_token"
+  end
+end
