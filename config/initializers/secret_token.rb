156a61d100768b8c5c31f2abed784339f5ec5022
Michael Morrissey <mikehmorrissey@gmail.com>
2013-03-14 16:10:21 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IolabsTaskManager::Application.config.secret_token = 'c940aa1177d27d8f3b5ff0cba71c5e06a340868028da581ad50e0a4da8408a19950a3af368e41c2f72917e36f12b524276756792ac064b50e1e1aadbfbfdf528'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+IolabsTaskManager::Application.config.session_store :cookie_store, key: '_iolabs_task_manager_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# IolabsTaskManager::Application.config.session_store :active_record_store
