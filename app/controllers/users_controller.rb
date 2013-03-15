51710f3eb1cf1ee3cf6795917030f361152c05d3
Michael Morrissey <mikehmorrissey@gmail.com>
2013-03-14 23:54:46 -0400

    misc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
-
+	before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  
+  	before_filter :correct_user,   only: [:edit, :update]
+	
 	def index
 		@user = User.all
+	end
+
+	def show 
+		@user = User.find(params[:id])
 	end
 
 	def new
@@ -9,10 +16,13 @@ class UsersController < ApplicationController
 	end
 
 	def create
+
 		@user = User.new(params[:user])
+
 		if @user.save
 			flash[:success] = "Welcome to iolabs"
-
+			sign_in @user
+			redirect_to @user
 		else
 			render 'new'
 		end
@@ -33,5 +43,12 @@ class UsersController < ApplicationController
 		flash[:sucess] = "User successfully detroyed"
 		render 'index'
 	end
+
+	private
+
+	def correct_user
+      @user = User.find(params[:id])
+      redirect_to root_path unless current_user?(@user)
+    end
 
 end
