d75a53410239d8b5ea5bb529c370fb4166c7f394
Michael Morrissey <mikehmorrissey@gmail.com>
2013-03-14 23:50:07 -0400

    add remember token

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,12 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation, :avatar
  	
  	has_secure_password
- 	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
- 	validates_attachment :avatar, 
-  :content_type => { :content_type => "image" },
-  :size => { :in => 0..100.kilobytes }
+ 	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "iolabs_default.jpeg"
+ 	validates_attachment :avatar, :content_type => { :content_type => "image" }, :size => { :in => 0..100.kilobytes }
+
+	before_save { |user| user.email = user.email.downcase }
+  	before_save :create_remember_token
+
 
  	Email_Regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -17,5 +19,11 @@ class User < ActiveRecord::Base
 
  	validates :password, presence: true
 
+
+ 	private
+ 	
+ 	def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 
 end
