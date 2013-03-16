e31a6448b4d8cad880f933a3a87ad43116b63f03
Michael Morrissey <mikehmorrissey@gmail.com>
2013-03-16 16:16:46 -0400

    add default scope/ presnce validation

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,12 @@
 class Note < ActiveRecord::Base
   attr_accessible :content
   belongs_to :user
+  before_save :save_line_break
+  validates :content, presence: true
+   default_scope order: 'notes.created_at DESC'
+
+  def save_line_break
+  	self.content.gsub!(/\n/, '<br/>')
+  	
+  end
 end
