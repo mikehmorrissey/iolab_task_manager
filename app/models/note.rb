bd59359bfa98a74af32654511c06792b94a98de0
Michael Morrissey <mikehmorrissey@gmail.com>
2013-03-16 14:48:31 -0400

    model work

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,8 +1,4 @@
 class Note < ActiveRecord::Base
   attr_accessible :content
-
-  validates :content, presence: true
-  validates :user_id, presence: true
-	
-	belongs_to :user
+  belongs_to :user
 end
