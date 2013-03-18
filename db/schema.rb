bf64fda03827ffae4bb2af03f90f1bc346aabe13
Michael Morrissey <mikehmorrissey@gmail.com>
2013-03-17 23:06:59 -0400

    add status to Task table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130318022817) do
+ActiveRecord::Schema.define(:version => 20130318030635) do
 
   create_table "companies", :force => true do |t|
     t.string   "name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(:version => 20130318022817) do
     t.integer  "user_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "status"
   end
 
   create_table "users", :force => true do |t|
